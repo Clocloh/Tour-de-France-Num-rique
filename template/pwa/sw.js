f25f31952dcc45d764ff3ce48d5e0f1e63a10cd4
Clocloh <39056570+Clocloh@users.noreply.github.com>
2020-05-04 16:34:45 +0200

    pwa

---
+++ template/pwa/sw.js
@@ -0,0 +1,37 @@
+var cacheName = 'hello-pwa';
+var filesToCache = [
+'./',
+'simulateur.html',
+'rechercher.html',
+'profil_etudiant.html',
+'profil_entreprise.html',
+'profil_ecole.html',
+'inscrip_etudiant.html',
+'inscrip_entreprise.html',
+'inscrip_ecole.html',
+'index.html',
+'connex.html',
+'chat.html',
+'ajout_annonce.html',
+'./inscrip.html',
+'./css/style.css',
+'./js/main_pwa.js'
+];
+/* Démarrer le service worker et mettre en cache tout le contenu de l'application
+*/
+self.addEventListener('install', function(e) {
+e.waitUntil(
+caches.open(cacheName).then(function(cache) {
+return cache.addAll(filesToCache);
+})
+);
+});
+/* Servir le contenu mis en cache si hors ligne */
+self.addEventListener('fetch', function(e) {
+e.respondWith(
+caches.match(e.request).then(function(response) {
+
+return response || fetch(e.request);
+})
+);
+});
